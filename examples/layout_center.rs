9d13eb77246ed3c011409c81ab0d83c4cc7e1041
David Zhang <david7a68@gmail.com>
2025-08-11 23:29:30 -0400

    move ui into base crate, implement basic framework

--- examples/layout_center.rs
+++ examples/layout_center.rs
@@ -1,35 +1,15 @@
-use std::sync::Arc;
-use std::time::Duration;
-
-use graphics::Canvas;
+use sabre::AppContextBuilder;
+use sabre::AppLifecycleHandler;
+use sabre::Context;
+use sabre::UiBuilder;
+use sabre::ViewportConfig;
 use sabre::graphics::Color;
-use sabre::graphics::GraphicsContext;
-use sabre::ui::UiContext;
-use sabre::ui::input::InputState;
-use smallvec::smallvec;
+use sabre::layout::Alignment;
+use sabre::layout::LayoutDirection;
+use sabre::layout::Padding;
 use tracing::Level;
-use tracing::info;
-use tracing::instrument;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
-use ui_base::DrawCommand;
-use ui_base::layout::Alignment;
-use ui_base::layout::LayoutDirection;
-use ui_base::layout::Padding;
-use ui_base::text::TextLayoutContext;
-use winit::application::ApplicationHandler;
-use winit::event::ElementState;
-use winit::event::MouseButton;
-use winit::event::WindowEvent;
-use winit::event_loop::ActiveEventLoop;
-use winit::event_loop::ControlFlow;
-use winit::event_loop::EventLoop;
-use winit::keyboard::Key;
-use winit::keyboard::NamedKey;
-use winit::platform::windows::EventLoopBuilderExtWindows;
-use winit::platform::windows::WindowAttributesExtWindows;
-use winit::window::Window;
-use winit::window::WindowId;
 
 fn main() {
     color_backtrace::install();
@@ -48,244 +28,63 @@ fn main() {
         .with(def_filter)
         .init();
 
-    App::new().run();
+    AppContextBuilder::default().run(App {});
 }
 
-struct AppWindow {
-    window: Arc<Window>,
+struct App {}
 
-    canvas: Canvas,
-    input: InputState,
-    ui_context: UiContext,
-}
-
-struct App {
-    graphics: Option<GraphicsContext>,
-    windows: Vec<AppWindow>,
-    text_layout_context: TextLayoutContext,
-}
-
-impl App {
-    fn new() -> Self {
-        Self {
-            graphics: None,
-            windows: vec![],
-            text_layout_context: TextLayoutContext::new(),
-        }
-    }
-}
-
-impl App {
-    fn run(mut self) {
-        let event_loop = EventLoop::builder().with_dpi_aware(true).build().unwrap();
-        event_loop.set_control_flow(ControlFlow::Wait);
-        event_loop.run_app(&mut self).unwrap();
-    }
-}
-
-impl ApplicationHandler for App {
-    #[instrument(skip_all)]
-    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        let window = Arc::new(
-            event_loop
-                .create_window(
-                    Window::default_attributes()
-                        .with_no_redirection_bitmap(false)
-                        .with_visible(false),
-                )
-                .unwrap(),
+impl AppLifecycleHandler for App {
+    fn resume(&mut self, runtime: &mut sabre::AppContext) {
+        runtime.create_viewport(
+            ViewportConfig {
+                title: "Sabre App".into(),
+                width: 800,
+                height: 600,
+            },
+            AppWindow::default().into_handler(),
         );
+    }
+}
 
-        let mut graphics_context = GraphicsContext::new(window.clone());
+#[derive(Default)]
+struct AppWindow {}
 
-        // Render to the window before showing it to avoid flashing when
-        // creating the window for the first time.
-        let mut canvas = graphics_context.create_canvas();
-        canvas.reset(Color::BLACK);
-        graphics_context
-            .render(smallvec![(window.id(), &canvas)])
-            .unwrap();
-
-        window.set_visible(true);
-
-        self.windows.push(AppWindow {
-            window,
-            canvas,
-            input: InputState::default(),
-            ui_context: UiContext::new(),
-        });
-        self.graphics = Some(graphics_context);
+impl AppWindow {
+    fn into_handler(mut self) -> impl FnMut(Context, UiBuilder) {
+        move |context, ui| self.update(context, ui)
     }
 
-    #[instrument(skip_all)]
-    fn suspended(&mut self, _: &ActiveEventLoop) {
-        self.windows.clear();
-        self.graphics = None;
-    }
-
-    #[instrument(skip(self, event_loop))]
-    fn window_event(
-        &mut self,
-        event_loop: &ActiveEventLoop,
-        window_id: WindowId,
-        event: WindowEvent,
-    ) {
-        match event {
-            WindowEvent::KeyboardInput {
-                device_id: _,
-                event,
-                is_synthetic: _,
-            } => {
-                if event.logical_key == Key::Named(NamedKey::Space) && event.state.is_pressed() {
-                    let window = self
-                        .windows
-                        .iter_mut()
-                        .find(|rc| rc.window.id() == window_id)
-                        .unwrap();
-
-                    window.window.request_redraw();
-                }
-
-                if event.logical_key == Key::Named(NamedKey::Escape) && event.state.is_pressed() {
-                    self.windows.retain(|rc| rc.window.id() != window_id);
-                }
-            }
-            WindowEvent::Resized(size) => {
-                let window = self
-                    .windows
-                    .iter_mut()
-                    .find(|rc| rc.window.id() == window_id)
-                    .unwrap();
-
-                window.input.window_size.width = size.width as f32;
-                window.input.window_size.height = size.height as f32;
-            }
-            WindowEvent::CloseRequested => {
-                self.windows.retain(|rc| rc.window.id() != window_id);
-                self.graphics.as_mut().unwrap().destroy_surface(window_id);
-            }
-            WindowEvent::CursorMoved {
-                device_id: _,
-                position,
-            } => {
-                let window = self
-                    .windows
-                    .iter_mut()
-                    .find(|rc| rc.window.id() == window_id)
-                    .unwrap();
-
-                window.input.pointer.x = position.x as f32;
-                window.input.pointer.y = position.y as f32;
-            }
-            WindowEvent::MouseInput {
-                device_id: _,
-                state,
-                button,
-            } => {
-                let window = self
-                    .windows
-                    .iter_mut()
-                    .find(|rc| rc.window.id() == window_id)
-                    .unwrap();
-
-                match (button, state) {
-                    (MouseButton::Left, ElementState::Pressed) => {
-                        window.input.mouse_state.is_left_down = true;
-                    }
-                    (MouseButton::Left, ElementState::Released) => {
-                        window.input.mouse_state.is_left_down = false;
-                    }
-                    (MouseButton::Right, ElementState::Pressed) => {
-                        window.input.mouse_state.is_right_down = true;
-                    }
-                    (MouseButton::Right, ElementState::Released) => {
-                        window.input.mouse_state.is_right_down = false;
-                    }
-                    (MouseButton::Middle, ElementState::Pressed) => {
-                        window.input.mouse_state.is_middle_down = true;
-                    }
-                    (MouseButton::Middle, ElementState::Released) => {
-                        window.input.mouse_state.is_middle_down = false;
-                    }
-                    _ => (),
-                }
-            }
-            WindowEvent::Destroyed => {
-                if self.windows.is_empty() {
-                    info!("All windows destroyed, shutting down.");
-                    event_loop.exit();
-                }
-            }
-            WindowEvent::RedrawRequested => {
-                let window = self
-                    .windows
-                    .iter_mut()
-                    .find(|rc| rc.window.id() == window_id)
-                    .unwrap();
-
-                let mut ui = window.ui_context.begin_frame(
-                    &mut self.text_layout_context,
-                    window.input.clone(),
-                    Duration::ZERO,
-                );
-
-                ui.color(Color::srgb(0.1, 0.2, 0.3, 1.0))
-                    .child_alignment(Alignment::Center, Alignment::Center)
+    fn update(&mut self, _context: Context, mut ui: UiBuilder) {
+        ui.color(Color::srgb(0.1, 0.2, 0.3, 1.0))
+            .child_alignment(Alignment::Center, Alignment::Center)
+            .with_container(|ui| {
+                ui.child_direction(LayoutDirection::Vertical)
                     .with_container(|ui| {
-                        ui.child_direction(LayoutDirection::Vertical)
-                            .with_container(|ui| {
-                                ui.child_spacing(10.0)
-                                    .padding(Padding {
-                                        left: 15.0,
-                                        right: 15.0,
-                                        top: 15.0,
-                                        bottom: 15.0,
-                                    })
-                                    .color(Color::BLUE)
-                                    .rect(100.0, 100.0, Color::WHITE)
-                                    .rect(100.0, 200.0, Color::WHITE)
-                                    .rect(30.0, 150.0, Color::WHITE);
+                        ui.child_spacing(10.0)
+                            .padding(Padding {
+                                left: 15.0,
+                                right: 15.0,
+                                top: 15.0,
+                                bottom: 15.0,
                             })
-                            .with_container(|ui| {
-                                ui.child_spacing(10.0)
-                                    .color(Color::GREEN)
-                                    .padding(Padding {
-                                        left: 15.0,
-                                        right: 15.0,
-                                        top: 15.0,
-                                        bottom: 15.0,
-                                    })
-                                    .rect(100.0, 91.0, Color::WHITE)
-                                    .rect(100.0, 15.0, Color::WHITE)
-                                    .rect(100.0, 299.0, Color::WHITE);
-                            });
+                            .color(Color::BLUE)
+                            .rect(100.0, 100.0, Color::WHITE)
+                            .rect(100.0, 200.0, Color::WHITE)
+                            .rect(30.0, 150.0, Color::WHITE);
+                    })
+                    .with_container(|ui| {
+                        ui.child_spacing(10.0)
+                            .color(Color::GREEN)
+                            .padding(Padding {
+                                left: 15.0,
+                                right: 15.0,
+                                top: 15.0,
+                                bottom: 15.0,
+                            })
+                            .rect(100.0, 91.0, Color::WHITE)
+                            .rect(100.0, 15.0, Color::WHITE)
+                            .rect(100.0, 299.0, Color::WHITE);
                     });
-
-                let graphics = self.graphics.as_mut().unwrap();
-                let canvas = &mut window.canvas;
-
-                canvas.reset(Color::srgb(0.1, 0.2, 0.3, 1.0));
-
-                for draw_command in window.ui_context.finish() {
-                    match draw_command {
-                        DrawCommand::Primitive(primitive) => {
-                            canvas.draw(primitive);
-                        }
-                        DrawCommand::TextLayout(layout, coords) => {
-                            canvas.draw_text_layout(layout, coords);
-                        }
-                    }
-                }
-
-                if canvas.has_unready_textures() {
-                    window.window.request_redraw();
-                }
-
-                graphics
-                    .render(smallvec![(window.window.id(), &*canvas)])
-                    .unwrap();
-            }
-            _ => (),
-        }
+            });
     }
 }
---
+++ src/runtime.rs
@@ -0,0 +1,323 @@
+use std::borrow::Cow;
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::time::Duration;
+
+use crate::DrawCommand;
+use crate::UiBuilder;
+use crate::input::InputState;
+use crate::text::TextLayoutContext;
+use crate::ui::UiContext;
+use graphics::Canvas;
+use graphics::Color;
+use graphics::GraphicsContext;
+use slotmap::SlotMap;
+use slotmap::new_key_type;
+use smallvec::SmallVec;
+use tracing::warn;
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::ActiveEventLoop;
+use winit::event_loop::ControlFlow;
+use winit::event_loop::EventLoop;
+use winit::event_loop::EventLoopProxy;
+use winit::platform::windows::EventLoopBuilderExtWindows;
+use winit::platform::windows::WindowAttributesExtWindows;
+use winit::window::Window;
+use winit::window::WindowId;
+
+#[derive(Default)]
+pub struct AppContextBuilder {}
+
+impl AppContextBuilder {
+    pub fn run(self, handler: impl AppLifecycleHandler) {
+        let event_loop = EventLoop::with_user_event()
+            .with_dpi_aware(true)
+            .build()
+            .unwrap();
+        event_loop.set_control_flow(ControlFlow::Wait);
+
+        let mut runtime = WinitApp {
+            runtime: AppContext {
+                viewports: SlotMap::with_key(),
+                deferred_commands: Vec::new(),
+                graphics: None,
+                text_system: TextLayoutContext::default(),
+                event_loop_proxy: event_loop.create_proxy(),
+            },
+            windows: HashMap::new(),
+            user_handler: handler,
+        };
+
+        event_loop.run_app(&mut runtime).unwrap();
+    }
+}
+
+pub trait AppLifecycleHandler {
+    fn suspend(&mut self, runtime: &mut AppContext) {
+        let _ = runtime;
+    }
+
+    fn resume(&mut self, runtime: &mut AppContext);
+}
+
+pub struct AppContext {
+    viewports: SlotMap<ViewportId, Viewport>,
+
+    deferred_commands: Vec<ViewportCommand>,
+
+    graphics: Option<GraphicsContext>,
+    text_system: TextLayoutContext,
+
+    event_loop_proxy: EventLoopProxy<WinitEvent>,
+}
+
+impl AppContext {
+    pub fn create_viewport(
+        &mut self,
+        config: ViewportConfig,
+        handler: impl FnMut(Context, UiBuilder) + 'static,
+    ) -> ViewportHandle {
+        let id = self.viewports.insert(Viewport {
+            input: InputState::default(),
+            config,
+        });
+
+        self.deferred_commands.push(ViewportCommand::Create {
+            id,
+            handler: Box::new(handler),
+        });
+
+        ViewportHandle {
+            id,
+            event_loop_proxy: self.event_loop_proxy.clone(),
+        }
+    }
+
+    fn repaint<'a>(
+        &mut self,
+        windows: impl IntoIterator<Item = (&'a mut WinitWindow, InputState)>,
+    ) {
+        let windows = windows.into_iter();
+        let mut outputs = SmallVec::with_capacity(windows.size_hint().0);
+
+        for (window, input) in windows {
+            let ui_builder =
+                window
+                    .ui_context
+                    .begin_frame(&mut self.text_system, input, Duration::ZERO);
+
+            let context = Context {
+                viewports: &mut self.viewports,
+                deferred_commands: &mut self.deferred_commands,
+                event_loop_proxy: &self.event_loop_proxy,
+            };
+
+            (window.handler)(context, ui_builder);
+
+            window.canvas.reset(Color::BLACK);
+
+            for draw_command in window.ui_context.finish() {
+                match draw_command {
+                    DrawCommand::Primitive(primitive) => {
+                        window.canvas.draw(primitive);
+                    }
+                    DrawCommand::TextLayout(layout, coords) => {
+                        window.canvas.draw_text_layout(layout, coords);
+                    }
+                }
+            }
+
+            if window.canvas.has_unready_textures() {
+                window.window.request_redraw();
+            }
+
+            outputs.push((window.window.id(), &window.canvas));
+        }
+
+        let graphics = self.graphics.as_mut().unwrap();
+        graphics.render(outputs).unwrap();
+    }
+}
+
+#[derive(Clone)]
+pub struct ViewportHandle {
+    id: ViewportId,
+    event_loop_proxy: EventLoopProxy<WinitEvent>,
+}
+
+impl ViewportHandle {
+    pub fn send_message(&self, _: ()) {
+        self.event_loop_proxy
+            .send_event(WinitEvent {
+                viewport_id: self.id,
+            })
+            .unwrap();
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct ViewportConfig {
+    pub title: Cow<'static, str>,
+    pub width: u32,
+    pub height: u32,
+}
+
+pub struct Context<'a> {
+    viewports: &'a mut SlotMap<ViewportId, Viewport>,
+    deferred_commands: &'a mut Vec<ViewportCommand>,
+    event_loop_proxy: &'a EventLoopProxy<WinitEvent>,
+}
+
+impl Context<'_> {
+    pub fn create_viewport(
+        &mut self,
+        config: ViewportConfig,
+        handler: impl FnMut(Context, UiBuilder) + 'static,
+    ) -> ViewportHandle {
+        let id = self.viewports.insert(Viewport {
+            input: InputState::default(),
+            config,
+        });
+
+        self.deferred_commands.push(ViewportCommand::Create {
+            id,
+            handler: Box::new(handler),
+        });
+
+        ViewportHandle {
+            id,
+            event_loop_proxy: self.event_loop_proxy.clone(),
+        }
+    }
+}
+
+struct WinitApp<App> {
+    runtime: AppContext,
+    windows: HashMap<WindowId, WinitWindow>,
+
+    user_handler: App,
+}
+
+impl<App> WinitApp<App> {
+    fn handle_deferred_commands(&mut self, event_loop: &ActiveEventLoop) {
+        for command in self.runtime.deferred_commands.drain(..) {
+            match command {
+                ViewportCommand::Create { id, handler } => {
+                    let window = Arc::new(
+                        event_loop
+                            .create_window(
+                                Window::default_attributes()
+                                    .with_no_redirection_bitmap(true)
+                                    .with_visible(false),
+                            )
+                            .unwrap(),
+                    );
+
+                    let graphics = self
+                        .runtime
+                        .graphics
+                        .get_or_insert_with(|| GraphicsContext::new(window.clone()));
+
+                    self.windows.insert(
+                        window.id(),
+                        WinitWindow {
+                            canvas: graphics.create_canvas(),
+                            handler,
+                            ui_context: UiContext::default(),
+                            viewport: id,
+                            window,
+                        },
+                    );
+                }
+            }
+        }
+
+        if self.runtime.viewports.is_empty() {
+            warn!("No open windows, terminating...");
+            event_loop.exit();
+        }
+    }
+}
+
+impl<App: AppLifecycleHandler> ApplicationHandler<WinitEvent> for WinitApp<App> {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.user_handler.resume(&mut self.runtime);
+        self.handle_deferred_commands(event_loop);
+
+        self.runtime.repaint(self.windows.values_mut().map(|w| {
+            w.window.set_visible(true);
+            (w, InputState::default())
+        }));
+    }
+
+    #[allow(unused_variables)]
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        match event {
+            WindowEvent::Resized(physical_size) => {
+                let window = self.windows.get(&window_id).unwrap();
+                let viewport = self.runtime.viewports.get_mut(window.viewport).unwrap();
+
+                viewport.config.width = physical_size.width;
+                viewport.config.height = physical_size.height;
+
+                viewport.input.window_size.width = physical_size.width as f32;
+                viewport.input.window_size.height = physical_size.height as f32;
+            }
+            WindowEvent::CloseRequested => {
+                let window = self.windows.remove(&window_id).unwrap();
+
+                self.runtime.viewports.remove(window.viewport);
+
+                let graphics = self.runtime.graphics.as_mut().unwrap();
+                graphics.destroy_surface(window_id);
+            }
+            WindowEvent::RedrawRequested => {
+                let window = self.windows.get_mut(&window_id).unwrap();
+                let viewport = self.runtime.viewports.get_mut(window.viewport).unwrap();
+                let input = viewport.input.clone();
+
+                self.runtime.repaint([(window, input)]);
+            }
+            _ => {}
+        }
+
+        self.handle_deferred_commands(event_loop);
+    }
+}
+
+new_key_type! {
+    struct ViewportId;
+}
+
+struct Viewport {
+    input: InputState,
+    config: ViewportConfig,
+}
+
+struct WinitWindow {
+    window: Arc<Window>,
+
+    canvas: Canvas,
+    viewport: ViewportId,
+    ui_context: UiContext,
+    handler: Box<dyn FnMut(Context, UiBuilder)>,
+}
+
+enum ViewportCommand {
+    Create {
+        id: ViewportId,
+        handler: Box<dyn FnMut(Context, UiBuilder)>,
+    },
+}
+
+#[derive(Debug)]
+struct WinitEvent {
+    #[expect(unused)]
+    viewport_id: ViewportId,
+}
