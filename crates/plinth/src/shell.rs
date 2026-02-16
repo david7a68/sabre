4c9a9901b244e878a5aa9b8ee865ae34f9fc95f6
David Zhang <david7a68@gmail.com>
2026-02-15 21:42:01 -0500

    Add file picker support and example (#84)
    Closes #83

--- crates/plinth/src/shell.rs
+++ crates/plinth/src/shell.rs
@@ -11,6 +11,8 @@ pub use app_context::AppContextBuilder;
 pub use app_context::AppLifecycleHandler;
 pub use clipboard::Clipboard;
 pub use frame::Context;
+pub use frame::FileDialog;
+pub use frame::FolderDialog;
 pub use input::ElementState;
 pub use input::Input;
 pub use input::KeyboardEvent;
--- crates/plinth/src/shell/frame.rs
+++ crates/plinth/src/shell/frame.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use slotmap::SlotMap;
 
 use crate::ui::UiBuilder;
@@ -7,6 +9,65 @@ use super::WindowConfig;
 use super::window::Viewport;
 use super::window::ViewportId;
 use super::winit::DeferredCommand;
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct FileDialog {
+    /// The title of the file dialog window.
+    pub title: String,
+    /// The starting file name for the file dialog.
+    pub initial_file: String,
+    /// The starting directory for the file dialog.
+    pub directory: String,
+    /// File extension filters for the file dialog. Each filter consists of a
+    /// name and a list of extensions.
+    ///
+    /// On MacOS, the name is ignored and all extensions are merged into a
+    /// single filter.
+    pub filters: Vec<(String, Vec<String>)>,
+}
+
+impl FileDialog {
+    fn builder(self, window: &dyn winit::window::Window) -> rfd::FileDialog {
+        let mut builder = rfd::FileDialog::new()
+            .set_title(self.title)
+            .set_directory(self.directory)
+            .set_file_name(self.initial_file);
+
+        for (name, extensions) in self.filters {
+            builder = builder.add_filter(&name, &extensions);
+        }
+
+        builder.set_parent(window)
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct FolderDialog {
+    /// The title of the folder dialog window.
+    pub title: String,
+    /// The starting directory for the folder dialog.
+    pub directory: String,
+    /// File extension filters for the file dialog. Each filter consists of a
+    /// name and a list of extensions.
+    ///
+    /// On MacOS, the name is ignored and all extensions are merged into a
+    /// single filter.
+    pub filters: Vec<(String, Vec<String>)>,
+}
+
+impl FolderDialog {
+    fn builder(self, window: &dyn winit::window::Window) -> rfd::FileDialog {
+        let mut builder = rfd::FileDialog::new()
+            .set_title(self.title)
+            .set_directory(self.directory);
+
+        for (name, extensions) in self.filters {
+            builder = builder.add_filter(&name, &extensions);
+        }
+
+        builder.set_parent(window)
+    }
+}
 
 pub struct Context<'a> {
     pub(super) window: &'a dyn winit::window::Window,
@@ -33,5 +94,21 @@ impl Context<'_> {
 
     pub fn request_repaint(&self) {
         self.window.request_redraw();
+    }
+
+    pub fn pick_file(&self, dialog: FileDialog) -> Option<PathBuf> {
+        dialog.builder(self.window).pick_file()
+    }
+
+    pub fn pick_files(&self, dialog: FileDialog) -> Option<Vec<PathBuf>> {
+        dialog.builder(self.window).pick_files()
+    }
+
+    pub fn pick_folder(&self, dialog: FolderDialog) -> Option<PathBuf> {
+        dialog.builder(self.window).pick_folder()
+    }
+
+    pub fn pick_folders(&self, dialog: FolderDialog) -> Option<Vec<PathBuf>> {
+        dialog.builder(self.window).pick_folders()
     }
 }
